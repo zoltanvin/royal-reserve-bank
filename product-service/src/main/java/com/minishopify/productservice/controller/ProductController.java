59003a16ef623a274b27c39c89c4182819a338cf
zoltanvin <lightningball@protonmail.com>
2023-04-16 14:22:27 +0200

    building product service

--- product-service/src/main/java/com/minishopify/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/minishopify/productservice/controller/ProductController.java
@@ -1,2 +1,32 @@
-package com.minishopify.productservice.controller;public class ProductController {
+package com.minishopify.productservice.controller;
+
+import com.minishopify.productservice.dto.ProductRequest;
+//import com.minishopify.productservice.dto.ProductResponse;
+import com.minishopify.productservice.dto.ProductResponse;
+import com.minishopify.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
 }
--- product-service/src/main/java/com/minishopify/productservice/service/ProductService.java
+++ product-service/src/main/java/com/minishopify/productservice/service/ProductService.java
@@ -1,2 +1,46 @@
-package com.minishopify.productservice.service;public class ProductService {
+package com.minishopify.productservice.service;
+
+import com.minishopify.productservice.dto.ProductRequest;
+//import com.minishopify.productservice.dto.ProductResponse;
+import com.minishopify.productservice.dto.ProductResponse;
+import com.minishopify.productservice.model.Product;
+import com.minishopify.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
 }
