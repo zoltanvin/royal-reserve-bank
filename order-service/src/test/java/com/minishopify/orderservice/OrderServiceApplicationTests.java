00e426beb43c3109a75d4d88d04c28050ba215a9
zoltanvin <lightningball@protonmail.com>
2023-04-16 22:17:05 +0200

    spring initializer

---
+++ order-service/src/test/java/com/minishopify/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minishopify.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
