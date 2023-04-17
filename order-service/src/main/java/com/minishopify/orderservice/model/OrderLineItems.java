bee2fa7848086d458d200a19de9f264cdeda0730
zoltanvin <lightningball@protonmail.com>
2023-04-17 09:47:58 +0200

    model

---
+++ order-service/src/main/java/com/minishopify/orderservice/model/OrderLineItems.java
@@ -0,0 +1,25 @@
+package com.minishopify.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "t_order_line_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
