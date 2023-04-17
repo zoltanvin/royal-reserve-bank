10119b93ee0b253ceb126719789d8968813dad91
zoltanvin <lightningball@protonmail.com>
2023-04-17 20:09:04 +0200

    microservice communication

--- inventory-service/src/main/java/com/minishopify/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/minishopify/inventoryservice/service/InventoryService.java
@@ -1,10 +1,13 @@
 package com.minishopify.inventoryservice.service;
 
+import com.minishopify.inventoryservice.dto.InventoryResponse;
 import com.minishopify.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -15,7 +18,13 @@ public class InventoryService {
 
     @Transactional(readOnly = true)
     @SneakyThrows
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                ).toList();
     }
 }
---
+++ order-service/src/main/java/com/minishopify/orderservice/config/WebClientConfig.java
@@ -0,0 +1,14 @@
+package com.minishopify.orderservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webclient(){
+        return WebClient.builder().build();
+    }
+}
