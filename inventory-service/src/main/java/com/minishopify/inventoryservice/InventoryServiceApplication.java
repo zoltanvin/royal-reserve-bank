4fb18a92e51c393e107fc7f4932cb80989b4e555
zoltanvin <lightningball@protonmail.com>
2023-04-17 12:20:26 +0200

    initializer

---
+++ inventory-service/src/main/java/com/minishopify/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.minishopify.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+}
---
+++ inventory-service/src/test/java/com/minishopify/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minishopify.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
