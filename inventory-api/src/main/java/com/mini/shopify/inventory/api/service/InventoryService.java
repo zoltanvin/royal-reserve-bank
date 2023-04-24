3c717e7b94f7552644da67988ad6a4b0b6a4297a
zoltanvin <lightningball@protonmail.com>
2023-04-24 17:22:23 +0200

    circuit breaker

--- inventory-api/src/main/java/com/mini/shopify/inventory/api/service/InventoryService.java
+++ inventory-api/src/main/java/com/mini/shopify/inventory/api/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.mini.shopify.inventory.api.service;
 import com.mini.shopify.inventory.api.dto.InventoryResponse;
 import com.mini.shopify.inventory.api.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+//import lombok.SneakyThrows;
+//import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,12 +12,17 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+//@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+//    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+//        log.info("Wait");
+//        Thread.sleep(10000);
+//        log.info("Wait ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                         InventoryResponse.builder()
--- order-api/src/main/java/com/mini/shopify/order/api/controller/OrderController.java
+++ order-api/src/main/java/com/mini/shopify/order/api/controller/OrderController.java
@@ -2,9 +2,14 @@ package com.mini.shopify.order.api.controller;
 
 import com.mini.shopify.order.api.dto.OrderRequest;
 import com.mini.shopify.order.api.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -15,8 +20,15 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name="inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully!";
+        return CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequest));
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(()-> "Oops! Something went wrong, please try again later!");
     }
 }
